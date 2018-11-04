0b5bddc876af7cdb5389a05dd2198ce1b296d114
naren2605 <naren26051991@gmail.com>
2018-11-04 21:01:21 +0530

    final changes

--- src/main/java/test/fission/contoller/UserDataController.java
+++ src/main/java/test/fission/contoller/UserDataController.java
@@ -5,31 +5,44 @@ import test.fission.service.UserDataService;
 import test.fission.service.UserDataServiceImpl;
 
 import java.util.InputMismatchException;
+import java.util.Scanner;
+import java.util.concurrent.BlockingDeque;
+import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingQueue;
 
 public class UserDataController {
     private final UserDataService userDataService;
-    private final ConcurrentLinkedQueue<String> concurrentLinkedQueue;
+    private final BlockingQueue<String> queue;
     public UserDataController(){
         this.userDataService=new UserDataServiceImpl();
-        concurrentLinkedQueue=new ConcurrentLinkedQueue<>();
-        new Thread(()->listenToQueue()).start();
+        queue=new LinkedBlockingQueue<>();
+
+        new Thread(()-> {
+            try {
+                listenToQueue();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }).start();
 
     }
 
-    public void listenToQueue(){
+    public void listenToQueue() throws InterruptedException {
         String command;
-        while((command=concurrentLinkedQueue.poll())!=null||(command!=null&&!command.equals("EXIT"))){
+        while((command=queue.take())!=null&&(command!=null&&!command.equals("EXIT"))){
             command(command);
         }
+        System.out.println("done...");
     }
 
     public void send(String command){
-        concurrentLinkedQueue.add(command);
+        queue.add(command);
     }
 
 
     public void command(String string){
+        string=string.trim();
         if(string!=null&&string.matches(" *.+, *.+, *[0-9]+, *[0-9]+, *.+")){
                 String[] values=string.split(",");
             User user =new User();
@@ -41,25 +54,31 @@ public class UserDataController {
             userDataService.add(user);
         }else if(string.equals("SORT")){
             userDataService.sort();
-        }else if(string.equals("EXIT")){
-            System.out.println("graceful exit...");
-            System.exit(0);
         }
         else{
             throw new InputMismatchException("please check the input and try again");
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         UserDataController userDataController=new UserDataController();
 
-        userDataController.send("Naveen, Chandra, 50, 25, Fission");
+       /* userDataController.send("Naveen, Chandra, 50, 25, Fission");
         userDataController.send("Virat, Kohli, 74, 55, HCL");
         userDataController.send("SORT");
         userDataController.send("Ashok, Verma, 17, 17, Fission");
         userDataController.send("Ashok, Verma, 16, 17, Fission");
         userDataController.send("SORT");
-        userDataController.send("EXIT");
+        userDataController.send("EXIT");*/
 
+        Scanner scanner =new Scanner(System.in);
+        while(scanner.hasNext()){
+            String command=scanner.nextLine();
+            userDataController.send(command);
+            if (command.equals("EXIT")){
+                break;
+            }
+
+        }
     }
 }
